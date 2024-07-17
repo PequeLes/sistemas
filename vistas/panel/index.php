ffddece265d417f218416e188301730b89da6382
DanielChamaco1 <vivalafifa2013@gmail.com>
2024-07-17 16:34:55 -0400

    recuperado

--- vistas/panel/index.php
+++ vistas/panel/index.php
@@ -15,10 +15,6 @@
 
 <body>
     <div class="wrapper">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f50e0c4e8bd34e6d59cc47aef7ed1fd575bc3ad2
     <?php
     include('./navbar.php');  
     ?>
@@ -406,222 +402,16 @@ $con=0;
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
        
-<<<<<<< HEAD
-=======
-        <aside id="sidebar">
-            <div class="d-flex">
-                <button class="toggle-btn" type="button">
-                    <i class="lni lni-grid-alt"></i>
-                </button>
-                <div class="sidebar-logo">
-                    <a href="./index.php" class="nav-link" data-bs-target="#inicio">Inicio</a>
-                </div>
-            </div>
-            <ul class="sidebar-nav">
-                <li class="sidebar-item">
-                    <a href="./perfil.php" class="sidebar-link nav-link" data-bs-target="#perfil">
-                        <i class="lni lni-user"></i>
-                        <span>Perfil</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="./pendientes.php" class="sidebar-link nav-link">
-                        <i class="lni lni-agenda"></i>
-                        <span>Pendientes</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="./autenticacion.php" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
-                        <i class="lni lni-protection"></i>
-                        <span>Autenticación</span>
-                    </a>
-                    <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                        <li class="sidebar-item">
-                            <a href="./registroauten/registro.php" class="sidebar-link">Registro</a>a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Rol</a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Permiso</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-item">
-                    <a href="./departamento.php" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
-                        <i class="lni lni-layout"></i>
-                        <span>Depeartamento</span>
-                    </a>
-                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-one" aria-expanded="false" aria-controls="multi-one">
-                                Dpto. l Personal
-                            </a>
-                            <ul id="multi-one" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                                Dpto ll Inteligencia
-                            </a>
-                            <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-three" aria-expanded="false" aria-controls="multi-three">
-                                Dpto lll Operaciones
-                            </a>
-                            <ul id="multi-three" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-four" aria-expanded="false" aria-controls="multi-four">
-                                Dpto lV Logistica
-                            </a>
-                            <ul id="multi-four" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-five" aria-expanded="false" aria-controls="multi-five">
-                                Dpto V Educaciones
-                            </a>
-                            <ul id="multi-five" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-six" aria-expanded="false" aria-controls="multi-six">
-                                Dpto VI Ciencia y Tecnologia
-                            </a>
-                            <ul id="multi-six" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
 
-                </li>
-                <li class="sidebar-item">
-                    <a href="./notificaciones.php" class="sidebar-link">
-                        <i class="lni lni-popup"></i>
-                        <span>Notificaciones</span>
-                    </a>
-                </li>
-
-            </ul>
-            <div class="sidebar-footer">
-                <a href="../../index.php" class="sidebar-link">
-                    <i class="lni lni-exit"></i>
-                    <span>Salida</span>
-                </a>
-            </div>
-        </aside>
-        <div class="main p-5">
-            <div id="inicio" class="text-center content-div">
-                <h1>
-                    Informática
-                </h1>
-
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Column 1</th>
-                            <th scope="col">Column 2</th>
-                            <th scope="col">Column 3</th>
-                            <th scope="col">Column 4</th>
-                            <th scope="col">Column 5</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Row 1, Cell 1</td>
-                            <td>Row 1, Cell 2</td>
-                            <td>Row 1, Cell 3</td>
-                            <td>Row 1, Cell 4</td>
-                            <td>Row 1, Cell 5</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Row 2, Cell 1</td>
-                            <td>Row 2, Cell 2</td>
-                            <td>Row 2, Cell 3</td>
-                            <td>Row 2, Cell 4</td>
-                            <td>Row 2, Cell 5</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Row 3, Cell 1</td>
-                            <td>Row 3, Cell 2</td>
-                            <td>Row 3, Cell 3</td>
-                            <td>Row 3, Cell 4</td>
-                            <td>Row 3, Cell 5</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Row 4, Cell 1</td>
-                            <td>Row 4, Cell 2</td>
-                            <td>Row 4, Cell 3</td>
-                            <td>Row 4, Cell 4</td>
-                            <td>Row 4, Cell 5</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">5</th>
-                            <td>Row 5, Cell 1</td>
-                            <td>Row 5, Cell 2</td>
-                            <td>Row 5, Cell 3</td>
-                            <td>Row 5, Cell 4</td>
-                            <td>Row 5, Cell 5</td>
-                        </tr>
-                    </tbody>
-
-
-                </table>
-            </div>
-
-        </div>
+       
+      
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
        
-=======
->>>>>>> f50e0c4e8bd34e6d59cc47aef7ed1fd575bc3ad2
+
     <script src="script.js"></script>
     <script>
         function cambiarEstado(id) {
